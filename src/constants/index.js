78cc7486c16f5b60aebc1a34099090f165d456d5
james65 <jamestonibor65@gmail.com>
2026-02-04 02:28:50 +0100

    my potfolio

---
+++ src/constants/index.js
@@ -0,0 +1,225 @@
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Skills",
+    link: "#experience",
+  },
+ 
+];
+
+const words = [
+  { text: "Imagination", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Imagination", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 5, suffix: "+", label: "Years of Experience" },
+  { value: 30, suffix: "+", label: "Satisfied Clients" },
+  { value: 50, suffix: "+", label: "Completed Projects" },
+  { value: 90, suffix: "%", label: "Client Retention Rate" },
+];
+
+const logoIconsList = [
+  {
+    logoPath: "/images/tWind.png",
+  },
+  {
+    logoPath: "/images/php_logo.png",
+  },
+  {
+    logoPath: "/images/js_logo.png",
+  },
+  {
+    logoPath: "/images/react_logo.png",
+  },
+  {
+    logoPath: "/images/jav_logo.png",
+  },
+  {
+    logoPath: "/images/sboot_logo.png",
+  },
+  {
+    logoPath: "/images/mysql_logo.png",
+  },
+  {
+    logoPath: "/images/github_logo.png",
+  },
+  {
+    logoPath: "/images/kafka_logo.png",
+  },
+ 
+];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Excellence",
+    desc: "Delivering excellent results while maintaining attention to every detail.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Reliable Communication",
+    desc: "Keeping you updated at every step to ensure transparency and clarity.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Delivery",
+    desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
+  },
+];
+
+const expCards = [
+  {
+    review: "Rated",
+    logoPath: "/images/tWind.png",
+    title: "Tailwind Css",
+    date: "2023 - Present",
+    responsibilities: [
+      "Front-end Design",
+      
+    ],
+  },
+  {
+    review: "Rated",
+    logoPath: "/images/php_logo.png",
+    title: "PHP",
+    date: "2023 - Present",
+    responsibilities: [
+      "used in web development primarily for creating dynamic web pages and applications.",
+     
+    ],
+  },
+  {
+    review: "Rated",
+    logoPath: "/images/js_logo.png",
+    title: "Java Script",
+    date: "2023 - Present",
+    responsibilities: [
+      " Front-end Web Development (Client-Side).",
+      "Back-end Web Development (Server-Side)",
+     
+    ],
+  },
+   {
+    review: "Rated",
+    logoPath: "/images/react_logo.png",
+    title: "ReactJs",
+    date: "2024 - Present",
+    responsibilities: [
+      " used primarily for building dynamic and interactive user interfaces (UIs) for web applications.",
+    ],
+  },
+   {
+    review: "Rated",
+    logoPath: "/images/jav_logo.png",
+    title: "Java",
+    date: "2022 - Present",
+    responsibilities: [
+      "used for server-side (backend) web development.",
+      "Microservices Architecture.",
+    ],
+  },
+   {
+    review: "Rated",
+    logoPath: "/images/sboot_logo.png",
+    title: "Spring Boot",
+    date: "2023 - Present",
+    responsibilities: [
+      "used in web development to streamline and speed up the creation of production-ready, stand-alone Spring-based applications with minimal configuration.",
+      "Spring Security (Authentication, Authorization and Session Management)",
+    ],
+  },
+     {
+    review: "Rated",
+    logoPath: "/images/mysql_logo.png",
+    title: "MySQL",
+    date: "2023 - Present",
+    responsibilities: [
+      "primarily employed for storing, managing, and retrieving data generated and consumed by web applications.",
+    ],
+  },
+  {
+    review: "Rated",
+    logoPath: "/images/g_hub.png",
+    title: "Github",
+    date: "2023 - Present",
+    responsibilities: [
+      "primarily used for tracking changes in source code, facilitating seamless collaboration among multiple developers, and enabling the easy management of different project.",
+    ],
+  },
+  {
+    review: "Rated",
+    logoPath: "/images/kafka_logo.png",
+    title: "Kafka",
+    date: "2024 - Present",
+    responsibilities: [
+      "Website Activity Tracking & Analytics.",
+      "Event-Driven Microservices Communication.",
+      "Real-Time Data Pipelines (ETL)",
+    ],
+  },
+  {
+    review: "Rated",
+    logoPath: "/images/ms_logo.png",
+    title: "Micro Services",
+    date: "2025 - Present",
+    responsibilities: [
+      "a design approach that structures an application as a collection of small, loosely coupled, and independently deployable services, each implementing a specific business function.",
+    ],
+  },
+];
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3",
+    imgPath: "/images/logo3.png",
+  },
+];
+
+
+const socialImgs = [
+  {
+    name: "insta",
+    imgPath: "/images/insta.png",
+  },
+  {
+    name: "fb",
+    imgPath: "/images/fb.png",
+  },
+  {
+    name: "x",
+    imgPath: "/images/x.png",
+  },
+  {
+    name: "linkedin",
+    imgPath: "/images/linkedin.png",
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  socialImgs,
+  navLinks,
+};
